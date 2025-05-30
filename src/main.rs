fc08431ee64391c89892bf9e12ede053decf7ba5
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-05-30 21:05:44 +0200

    move the code inside the stream's for loop into a handle_connection function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{BufReader, Read, Write},
-    net::{SocketAddr, TcpListener},
+    net::{SocketAddr, TcpListener, TcpStream},
 };
 
 const END_OF_LINE: &str = "\r\n";
@@ -12,51 +12,55 @@ fn main() {
 
     println!("Listening to requests: {address}");
 
-    // We are now listening to any incoming request
+    // We are now listening to any incoming connection
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        // For faster/more efficient reading of stream
-        let buf_reader = BufReader::new(stream.try_clone().unwrap());
-
-        // Our buffer for reading any data sent by the client, aka the HTTP request
-        let mut http_request = String::new();
-
-        // We read byte by byte and we push each character into the buffer
-        for byte in buf_reader.bytes() {
-            let byte = byte.unwrap();
-            let char = byte as char;
-            http_request.push(char);
-
-            // If character is \n and the request ends with \r\n\r\n, we stop reading
-            if char == '\n' && http_request.ends_with("\r\n\r\n") {
-                break;
-            }
-        }
-        println!("{http_request:?}");
-
-        // The body of the HTTP response
-        let message_body = "uwu".to_string();
-        // The response status
-        let status = b"HTTP/1.1 200 OK\r\n";
-        // The response headers
-        let headers = [
-            "Content-Type: text/html".into(),
-            format!("Content-Length: {}", message_body.len()),
-        ];
-
-        // The response buffer
-        let mut response = Vec::new();
-        // We write the status in this buffer/array of u8s
-        response.write_all(status).unwrap();
-        // Now the headers
-        let header_string = headers.join("\r\n") + END_OF_LINE;
-        response.write_all(header_string.as_bytes()).unwrap();
-        // The mandatory empty line separating the headers and the message body
-        response.write_all(END_OF_LINE.as_bytes()).unwrap();
-        // And now we write the message body
-        response.write_all(message_body.as_bytes()).unwrap();
-
-        // We write (or send) the entire response to the stream
-        stream.write_all(&response).unwrap();
+        handle_connection(&mut stream);
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    // For faster/more efficient reading of stream
+    let buf_reader = BufReader::new(stream.try_clone().unwrap());
+
+    // Our buffer for reading any data sent by the client
+    let mut http_request = String::new();
+
+    // We read byte by byte and we push each character into the buffer
+    for byte in buf_reader.bytes() {
+        let byte = byte.unwrap();
+        let char = byte as char;
+        http_request.push(char);
+
+        // If character is \n and the request ends with \r\n\r\n, we stop reading
+        if char == '\n' && http_request.ends_with("\r\n\r\n") {
+            break;
+        }
+    }
+    println!("{http_request:?}");
+
+    // The body of the HTTP response
+    let message_body = "uwu".to_string();
+    // The response status
+    let status = b"HTTP/1.1 200 OK\r\n";
+    // The response headers
+    let headers = [
+        "Content-Type: text/html".into(),
+        format!("Content-Length: {}", message_body.len()),
+    ];
+
+    // The response buffer
+    let mut response = Vec::new();
+    // We write the status in this buffer/array of u8s
+    response.write_all(status).unwrap();
+    // Now the headers
+    let header_string = headers.join("\r\n") + END_OF_LINE;
+    response.write_all(header_string.as_bytes()).unwrap();
+    // The mandatory empty line separating the headers and the message body
+    response.write_all(END_OF_LINE.as_bytes()).unwrap();
+    // And now we write the message body
+    response.write_all(message_body.as_bytes()).unwrap();
+
+    // We write (or send) the entire response to the stream
+    stream.write_all(&response).unwrap();
 }
